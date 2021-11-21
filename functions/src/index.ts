d048adb0d8cec248601daf03a4f9974cddd3f1b7
OxyTom <oxytom8@gmail.com>
2021-11-21 22:38:33 +0100

    perf: :zap: Optimisations on firebase cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,40 +10,49 @@ initializeApp()
 const store = firestore()
 
 const resetCategoriesBalance = async (
-  user: firestore.DocumentSnapshot<firestore.DocumentData>,
+  ref: firestore.DocumentReference<firestore.DocumentData>,
   d: Date
 ) => {
-  if (d > (user.get('resetDate') as firestore.Timestamp).toDate()) {
+  if (
+    d > ((await ref.get()).get('resetDate') as firestore.Timestamp).toDate()
+  ) {
     const nd = new Date(d)
     nd.setMonth(d.getMonth() + 1)
-    user.ref.set(
+    await ref.set(
       {
         resetDate: firestore.Timestamp.fromDate(nd),
       },
       { merge: true }
     )
-    const accounts = await user.ref.collection('accounts').listDocuments()
-    for (const adoc of accounts) {
-      const categories = await adoc.collection('categories').listDocuments()
-      for (const cdoc of categories) {
-        cdoc.set(
-          {
-            balance: 0,
-          },
-          { merge: true }
+    await Promise.all(
+      // for (cosnt aref of await ref.collection('accounts').listDocuments())
+      (
+        await ref.collection('accounts').listDocuments()
+      ).map(async (aref) => {
+        // for (cosnt cref of await aref.collection('categories').listDocuments()
+        return Promise.all(
+          (await aref.collection('categories').listDocuments()).map((cref) => {
+            return cref.set(
+              {
+                balance: 0,
+              },
+              { merge: true }
+            )
+          })
         )
-      }
-    }
+      })
+    )
   }
 }
 
 const checkUsers = async () => {
   const d = new Date()
   const users = await store.collection('users').listDocuments()
-  for (const udoc of users) {
-    const user = await udoc.get()
-    resetCategoriesBalance(user, d)
-  }
+  Promise.all([
+    ...users.map(async (ref) => {
+      await resetCategoriesBalance(ref, d)
+    }),
+  ])
 
   return null
 }
@@ -52,34 +61,23 @@ const onOperationWrite = async (
   { after, before }: Change<firefnc.DocumentSnapshot>,
   { params }: EventContext
 ) => {
-  const aref = before.ref.firestore.doc(
-    `users/${params.userId}/accounts/${params.accountId}`
-  )
+  const promises = []
   const amount = {
     old: before.data()?.amount ?? 0,
     new: after.data()?.amount ?? 0,
-  }
-  // Update account balance
-  if (amount.old || amount.new) {
-    if (amount.old - amount.new !== 0) {
-      await aref.set(
-        {
-          balance: firestore.FieldValue.increment(amount.new - amount.old),
-        },
-        { merge: true }
-      )
-    }
   }
   // Update old category balance
   if (before.exists && (amount.old || before.data()?.category)) {
     const ref = before.data()
       ?.category as firestore.DocumentReference<firestore.DocumentData>
     if (ref) {
-      await ref.set(
-        {
-          balance: firestore.FieldValue.increment(amount.old),
-        },
-        { merge: true }
+      promises.push(
+        ref.set(
+          {
+            balance: firestore.FieldValue.increment(amount.old),
+          },
+          { merge: true }
+        )
       )
     }
   }
@@ -88,33 +86,41 @@ const onOperationWrite = async (
     const ref = after.data()
       ?.category as firestore.DocumentReference<firestore.DocumentData>
     if (ref) {
-      await ref.set(
-        {
-          balance: firestore.FieldValue.increment(-amount.new),
-        },
-        { merge: true }
+      promises.push(
+        ref.set(
+          {
+            balance: firestore.FieldValue.increment(-amount.new),
+          },
+          { merge: true }
+        )
       )
     }
   }
-  // Update counts
+  // Update counts & account balance
+  const ref = before.ref.firestore.doc(
+    `users/${params.userId}/accounts/${params.accountId}`
+  )
+  const data: {
+    balance?: firestore.FieldValue
+    operationCount?: firestore.FieldValue
+  } = {}
+  if ((amount.old || amount.new) && amount.old - amount.new !== 0) {
+    data.balance = firestore.FieldValue.increment(amount.new - amount.old)
+  }
+  let inc = 0
   if (before.exists && !after.exists) {
     // Deletion
-    await aref.set(
-      {
-        operationCount: firestore.FieldValue.increment(-1),
-      },
-      { merge: true }
-    )
+    inc = -1
   } else if (!before.exists && after.exists) {
     // Creation
-    await aref.set(
-      {
-        operationCount: firestore.FieldValue.increment(1),
-      },
-      { merge: true }
-    )
+    inc = 1
   }
+  if (inc) {
+    data.operationCount = firestore.FieldValue.increment(inc)
+  }
+  promises.push(ref.set(data, { merge: true }))
 
+  await Promise.all(promises)
   return null
 }
 
