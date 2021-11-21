6c1f054445db9e984f0f67f76c61f8974f236547
OxyTom <oxytom8@gmail.com>
2021-11-21 20:57:20 +0100

    feat: :zap: Added fonts to cache

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Surveillez vos économies de façon simple et efficace',
-      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -141,11 +136,20 @@ export default {
       twitterSite: 'https://comptes-oxy.web.app/',
       tiwtterCreator: '@OxyT0m8',
     },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: 'https://fonts.gstatic.com/*',
+          handler: 'staleWhileRevalidate',
+        },
+      ],
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
     theme: {
       dark: true,
       themes: {
