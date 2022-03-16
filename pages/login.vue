8b18b44a3522af5280d5fa3674f74376a3d69040
oxypomme <oxytom8@gmail.com>
2022-03-16 11:10:07 +0100

    feat: :lipstick: Better login page on mobile

--- pages/login.vue
+++ pages/login.vue
@@ -3,14 +3,20 @@
     <v-row justify="center">
       <v-col lg="6">
         <v-row>
-          <v-col cols="12" sm="6">
+          <v-col cols="12" sm="6" :order="showLogin ? 1 : 2">
             <v-card>
-              <Login />
+              <Login v-if="showLogin" />
+              <v-btn v-else block color="primary" @click="showLogin = true">
+                Déja inscrit ?
+              </v-btn>
             </v-card>
           </v-col>
-          <v-col cols="12" sm="6">
+          <v-col cols="12" sm="6" :order="showRegister ? 1 : 2">
             <v-card>
-              <Register />
+              <Register v-if="showRegister" />
+              <v-btn v-else block color="primary" @click="showRegister = true">
+                Pas encore inscrit ?
+              </v-btn>
             </v-card>
           </v-col>
         </v-row>
@@ -22,8 +28,29 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+  data: () => ({
+    rawShowRegister: false,
+  }),
   head: {
     title: 'Connexion - ',
+  },
+  computed: {
+    showRegister: {
+      get(): boolean {
+        return !this.$vuetify.breakpoint.xsOnly || this.rawShowRegister
+      },
+      set(value: boolean) {
+        this.rawShowRegister = value
+      },
+    },
+    showLogin: {
+      get(): boolean {
+        return !this.$vuetify.breakpoint.xsOnly || !this.rawShowRegister
+      },
+      set(value: boolean) {
+        this.rawShowRegister = !value
+      },
+    },
   },
   mounted() {
     // Update title
